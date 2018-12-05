45cf527a7582ec5d4fd775dbbbb8a65070a185a0
Nikolay Apanasik <apn2qq@gmail.com>
2018-12-05 14:22:55 +0300

    Added dynamic page query

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,25 @@
 const path = require('path')
 
-exports.createPages = ({ grapql, actions: { createPage } }) => {
-  createPage({
-    path: '/somefakepage',
-    component: path.resolve('./src/components/PostLayout.js'),
+exports.createPages = ({ graphql, actions: { createPage } }) => {
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(({ data: { allMarkdownRemark: { edges } } }) => {
+    edges.forEach(({ node: { frontmatter: { slug } } }) => {
+      createPage({
+        path: `/posts${slug}`,
+        component: path.resolve('./src/components/PostLayout.js'),
+        context: { slug },
+      })
+    })
   })
 }
--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import { graphql } from 'gatsby'
 import Layout from './layout'
+
+// StaticQuery
+// Used anyware, doesn't accept vars, can't use context
+
+// Page Query
+// Must be used on pages
 
 class PostLayout extends Component {
   render() {
     return (
       <Layout>
-        {console.log(this.props)}
         <h1>Privet</h1>
       </Layout>
     )
@@ -13,3 +19,16 @@ class PostLayout extends Component {
 }
 
 export default PostLayout
+
+export const query = graphql`
+  query PostLayout {
+    markdownRemark(frontmatter: { slug: { eq: "/second-post" } }) {
+      html
+      frontmatter {
+        title
+        slug
+        date
+      }
+    }
+  }
+`
